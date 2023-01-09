a97ce714d2a54f323a61ca49f27a1241901111ac
Kinda Autumn <kindaautumn@MacBook-Pro-de-Kinda.local>
2023-01-09 02:14:41 +0100

    Ajout de nouveaux fichiers

--- Graphe_Projet.cpp
+++ Graphe_Projet.cpp
@@ -1,57 +1,31 @@
-#include "Graphe19_N.hpp"
 #include <iostream>
+
+#include "Graphe.hpp"
+#include "Sudoku.hpp"
+
+#include <vector>
+#include <string>
+
+using namespace std;
 
 int main(){
 
-
-/*
-std::vector< std::pair<int,int> > arr(0);
-std::pair <int,int> arr1(0,1);
-std::pair <int,int> arr2(1,2);
-std::pair <int,int> arr3(2,3);
-std::pair <int,int> arr4(3,0);
-std::pair <int,int> arr5(1,3);
-std::pair <int,int> arr6(0,2);
-arr.push_back(arr1);
-arr.push_back(arr2);
-arr.push_back(arr3);
-arr.push_back(arr4);
-arr.push_back(arr5);
-arr.push_back(arr6);
-std::vector<int> v({0,1,2,3});
-Graphe_couleur G(v,arr,4) ;
-//std::vector<int> vect({-1,-1,1,-1});
-//G.coloration(vect);
-G.backtracking();
-G[0];
-G[1];
-G[2];
-G[3];
-   */
-
-
-/*
-for(int k=0;k<G.color_disp(0).size();k++){std::cout << G.color_disp(0)[k] << std::endl;}
-std::cout << "nb couleur" << G.nb_color_disp(0) << std::endl; */
-
-
-
-std::vector< std::pair<int,int> > arr(0);
-std::pair <int,int> arr1(0,2);
-std::pair <int,int> arr2(0,3);
-std::pair <int,int> arr3(0,5);
-std::pair <int,int> arr4(1,3);
-std::pair <int,int> arr5(1,4);
-std::pair <int,int> arr6(1,6);
-std::pair <int,int> arr7(2,4);
-std::pair <int,int> arr8(2,7);
-std::pair <int,int> arr9(3,8);
-std::pair <int,int> arr10(4,9);
-std::pair <int,int> arr11(5,6);
-std::pair <int,int> arr12(5,9);
-std::pair <int,int> arr13(6,7);
-std::pair <int,int> arr14(7,8);
-std::pair <int,int> arr15(8,9);
+vector< pair<int,int> > arr(0);
+pair <int,int> arr1(0,2);
+pair <int,int> arr2(0,3);
+pair <int,int> arr3(0,5);
+pair <int,int> arr4(1,3);
+pair <int,int> arr5(1,4);
+pair <int,int> arr6(1,6);
+pair <int,int> arr7(2,4);
+pair <int,int> arr8(2,7);
+pair <int,int> arr9(3,8);
+pair <int,int> arr10(4,9);
+pair <int,int> arr11(5,6);
+pair <int,int> arr12(5,9);
+pair <int,int> arr13(6,7);
+pair <int,int> arr14(7,8);
+pair <int,int> arr15(8,9);
 arr.push_back(arr1);
 arr.push_back(arr2);
 arr.push_back(arr3);
@@ -68,45 +42,45 @@ arr.push_back(arr13);
 arr.push_back(arr14);
 arr.push_back(arr15);
 
-std::vector<int> v({0,1,2,3,4,5,6,7,8,9});
+vector<int> v({0,1,2,3,4,5,6,7,8,9});
 
 Graphe_couleur Petersen(v,arr,3) ;
 
 Petersen[3] ; Petersen[9] ;
 
 //avec coloration
-//std::vector<int> vect({0,-1,-1,-1,-1,-1,-1,-1,-1});
+//vector<int> vect({0,-1,-1,-1,-1,-1,-1,-1,-1});
 //Petersen.coloration(vect);
 
 
 Petersen.backtracking();
 
-std::cout << "VERIFICATION" << std::endl;
+cout << "VERIFICATION" << endl;
 
 Petersen[0] ; Petersen[1] ; Petersen[2] ;Petersen[3] ;Petersen[4] ; 
 
 
 
 /*
-std::vector< std::pair<int,int> > arr(0);
-std::pair <int,int> arr1(0,1);
-std::pair <int,int> arr2(1,2);
-std::pair <int,int> arr3(2,0);
-std::pair <int,int> arr4(0,3);
-std::pair <int,int> arr5(1,5);
-std::pair <int,int> arr6(2,7);
-std::pair <int,int> arr7(3,4);
-std::pair <int,int> arr8(4,5);
-std::pair <int,int> arr9(5,6);
-std::pair <int,int> arr10(6,7);
-std::pair <int,int> arr11(7,8);
-std::pair <int,int> arr12(8,3);
-std::pair <int,int> arr13(9,3);
-std::pair <int,int> arr14(9,4);
-std::pair <int,int> arr15(9,5);
-std::pair <int,int> arr16(9,6);
-std::pair <int,int> arr17(9,7);
-std::pair <int,int> arr18(9,8);
+vector< pair<int,int> > arr(0);
+pair <int,int> arr1(0,1);
+pair <int,int> arr2(1,2);
+pair <int,int> arr3(2,0);
+pair <int,int> arr4(0,3);
+pair <int,int> arr5(1,5);
+pair <int,int> arr6(2,7);
+pair <int,int> arr7(3,4);
+pair <int,int> arr8(4,5);
+pair <int,int> arr9(5,6);
+pair <int,int> arr10(6,7);
+pair <int,int> arr11(7,8);
+pair <int,int> arr12(8,3);
+pair <int,int> arr13(9,3);
+pair <int,int> arr14(9,4);
+pair <int,int> arr15(9,5);
+pair <int,int> arr16(9,6);
+pair <int,int> arr17(9,7);
+pair <int,int> arr18(9,8);
 arr.push_back(arr1);
 arr.push_back(arr2);
 arr.push_back(arr3);
@@ -125,32 +99,32 @@ arr.push_back(arr15);
 arr.push_back(arr16);
 arr.push_back(arr17);
 arr.push_back(arr18);
-std::vector<int> v({0,1,2,3,4,5,6,7,8,9});
+vector<int> v({0,1,2,3,4,5,6,7,8,9});
 Graphe_couleur Golomb(v,arr,4) ;
 Golomb[9];
 Golomb[3];
 Golomb[0];
 Golomb[4];
 Golomb.backtracking();
-std::cout << "VERIFICATION" << std::endl; 
+cout << "VERIFICATION" << endl; 
 */
 
 /*
-std::vector< std::pair<int,int> > arr(0);
-std::pair <int,int> arr1(2,1);
-std::pair <int,int> arr2(1,3);
-std::pair <int,int> arr3(3,4);
-std::pair <int,int> arr4(4,2);
-std::pair <int,int> arr5(2,3);
-std::pair <int,int> arr6(1,4);
-std::pair <int,int> arr7(2,0);
-std::pair <int,int> arr8(0,1);
-std::pair <int,int> arr9(1,5);
-std::pair <int,int> arr10(5,6);
-std::pair <int,int> arr11(6,2);
-std::pair <int,int> arr12(6,4);
-std::pair <int,int> arr13(4,5);
-std::pair <int,int> arr14(5,3);
+vector< pair<int,int> > arr(0);
+pair <int,int> arr1(2,1);
+pair <int,int> arr2(1,3);
+pair <int,int> arr3(3,4);
+pair <int,int> arr4(4,2);
+pair <int,int> arr5(2,3);
+pair <int,int> arr6(1,4);
+pair <int,int> arr7(2,0);
+pair <int,int> arr8(0,1);
+pair <int,int> arr9(1,5);
+pair <int,int> arr10(5,6);
+pair <int,int> arr11(6,2);
+pair <int,int> arr12(6,4);
+pair <int,int> arr13(4,5);
+pair <int,int> arr14(5,3);
 arr.push_back(arr1);
 arr.push_back(arr2);
 arr.push_back(arr3);
@@ -165,7 +139,7 @@ arr.push_back(arr11);
 arr.push_back(arr12);
 arr.push_back(arr13);
 arr.push_back(arr14);
-std::vector<int> v({0,1,2,3,4,5,6});
+vector<int> v({0,1,2,3,4,5,6});
 Graphe_couleur test_3(v,arr,5);
 test_3.backtracking(); 
 
@@ -175,24 +149,24 @@ for(int k=0;k<7;k++){test_3[k];} */
 // TEST SUDOKU
 
 /*
-std::vector<int> numerotation({-1,-1,-1,0,2,-1,3,-1,-1,2,-1,-1,1,-1,-1,-1});
-Mini_sudoku sudoku(4,numerotation);
+vector<int> numerotation({-1,-1,-1,0,2,-1,3,-1,-1,2,-1,-1,1,-1,-1,-1});
+Sudoku sudoku(4,numerotation);
 
 int siz=(sudoku.liste_arretes()).size();
-for(int k=0;k<siz;k++){std::cout << (sudoku.liste_arretes())[k].first <<" - " << (sudoku.liste_arretes())[k].second << std::endl;
-std::cout << "arrete numero " << k << std::endl;}
+for(int k=0;k<siz;k++){cout << (sudoku.liste_arretes())[k].first <<" - " << (sudoku.liste_arretes())[k].second << endl;
+cout << "arrete numero " << k << endl;}
 
 
 sudoku.backtracking();
 // Appel de la méthode, qui modifie l'objet sudoku
 
-std::cout << "AFFICHAGE DES SOMMETS" << std::endl;
+cout << "AFFICHAGE DES SOMMETS" << endl;
 
 sudoku.affichage_sudoku(); */
 
 
 /*
-std::vector<int> numerotation(81,-1);
+vector<int> numerotation(81,-1);
 numerotation[1]=6;
 numerotation[2]=1;
 numerotation[5]=3;
@@ -233,16 +207,16 @@ numerotation[71]=0;
 numerotation[76]=1;
 numerotation[77]=4;
 numerotation[78]=6;  
-Mini_sudoku sudoku(9,numerotation);
+Sudoku sudoku(9,numerotation);
 
-std::cout << "taille de numerotation" << numerotation.size() << std::endl;
+cout << "taille de numerotation" << numerotation.size() << endl;
 
 sudoku.affichage_sudoku();
 
  sudoku.backtracking();
 // Appel de la méthode, qui modifie l'objet sudoku
 
-std::cout << "AFFICHAGE DES SOMMETS" << std::endl;
+cout << "AFFICHAGE DES SOMMETS" << endl;
 
 sudoku.affichage_sudoku(); 
 
@@ -252,7 +226,7 @@ sudoku.affichage_sudoku();
 
 
 /*
-std::vector<int> numerotation(81,-1);
+vector<int> numerotation(81,-1);
 numerotation[2]=6;
 numerotation[5]=1;
 numerotation[6]=7;
@@ -291,20 +265,21 @@ numerotation[72]=7;
 numerotation[74]=2;
 numerotation[75]=5;
 numerotation[78]=8; 
-Mini_sudoku sudoku(9,numerotation);
+Sudoku sudoku(9,numerotation);
 
-std::cout << "taille de numerotation" << numerotation.size() << std::endl;
+cout << "taille de numerotation" << numerotation.size() << endl;
 
 sudoku.affichage_sudoku();
 
  sudoku.backtracking();
 // Appel de la méthode, qui modifie l'objet sudoku
 
-std::cout << "AFFICHAGE DES SOMMETS" << std::endl;
+cout << "AFFICHAGE DES SOMMETS" << std::endl;
 
 sudoku.affichage_sudoku(); 
 
 // sudoku.affichage_couleurs();   */
 
 
-return 0;}
+return 0;
+}
